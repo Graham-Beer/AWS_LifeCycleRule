3d3fb19746ce391a32cb5b36cf0f98b7be327c7d
Graham Beer <graham.beer@outlook.com>
2018-12-15 16:51:51 +0000

    Create Get-BucketLifeCycle.ps1

---
+++ Get-BucketLifeCycle.ps1
@@ -0,0 +1,32 @@
+function Get-BucketLifeCycle {
+    [cmdletbinding(DefaultParameterSetName = 'FromPipeline')]
+    param (
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline, ParameterSetName = 'FromPipeline')]
+        [Amazon.S3.Model.S3Bucket] $S3Bucket,
+
+        [Parameter(Mandatory, Position = 0, ParameterSetName = 'ByBucketString')]
+        [String] $BucketByString
+    )
+    begin {
+        if ($pscmdlet.ParameterSetName -eq 'ByBucketString') {
+            $S3Bucket = Get-S3Bucket -BucketName $BucketByString
+        }
+    }
+    process {
+        $bucketRules = $S3Bucket | Get-S3LifecycleConfiguration
+
+        foreach ($bucketRule in $bucketRules.Rules) {
+            [PSCustomObject]@{
+                RuleName          = $bucketRule.Id
+                PrefixPath        = $bucketRule.Filter.LifecycleFilterPredicate.Prefix
+                DateTimeExpire    = $bucketRule.Expiration.Date
+                DaysUntilArchieve = $bucketRule.Expiration.Days
+                NoncurrentExpire  = $bucketRule.NoncurrentVersionExpiration.NoncurrentDays
+                StatusOfRule      = $bucketRule.Status.Value
+            }
+        }
+    }
+}
+
+## Example
+Get-S3Bucket -BucketName demo-life-cycle | Get-BucketLifeCycle
